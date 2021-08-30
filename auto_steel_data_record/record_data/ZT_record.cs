9f93153a5c4541788fa03df0708eac58786f05d0
hezongfanghuaok <754376164@qq.com>
2021-08-30 09:05:30 +0800

    update auto data record

--- auto_steel_data_record/record_data/ZT_record.cs
+++ auto_steel_data_record/record_data/ZT_record.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading;
 using S7.Net;
 using logtest;
+using System.Diagnostics;
+
 namespace record_data
 {
    public class ZT_record
@@ -14,9 +16,13 @@ namespace record_data
         public static bool back_low_speed;
         public static bool fall_high_speed;
         public static bool fall_low_speed;
-        public static bool TB_onpos=true;
-        public static float weight=0;
+        public static bool TB_onpos=true;//铁包到位信号
+        public static float weight=0;//铁包时时重量
         public static float lastweight=0;
+        public static float realangle = 0;//罐车倾角
+        public static float iron_hight = 0;//铁水包液面高度
+        public static float fish_full_weight = 0;//鱼雷罐满罐重量
+
 
         public void open_plc()
         {
@@ -37,38 +43,62 @@ namespace record_data
         {
             while (true)
             {
-                Thread.Sleep(1000);
+                //Thread.Sleep(500);
+                Stopwatch sw = new Stopwatch();
+
+
+                sw.Start();
                 back_high_speed = (bool)ZT_plc.Read("M4.3");
+                
                 back_low_speed = (bool)ZT_plc.Read("M4.1");
                 fall_high_speed = (bool)ZT_plc.Read("M3.7");
-                fall_low_speed = (bool)ZT_plc.Read("M3.5");
-               // TB_onpos=(bool)ZT_plc.Read("M3.6");
+                fall_low_speed = (bool)ZT_plc.Read("M3.5"); 
+                TB_onpos=(bool)ZT_plc.Read("I11.6");
                 lastweight = weight;
-                weight = (float)ZT_plc.Read(" DB80.DBD4");
-                if ((bool)back_high_speed == false)//speed=-80hz
-                {
 
-                    string log = "-80" + "\",\"" + weight.ToString();
+                //weight = ((uint)ZT_plc.Read("DB80.DBD4")).ConvertToFloat();
+                //realangle = ((uint)ZT_plc.Read("DB80.DBD74")).ConvertToFloat();
+                //fish_full_weight = ((uint)ZT_plc.Read("DB80.DBD78")).ConvertToFloat();
+               
+                var bytes = ZT_plc.ReadBytes(DataType.DataBlock, 80, 4, 4);
+               
+                weight = (float)S7.Net.Types.Double.FromByteArray(bytes);
+                
+                var bytes1 = ZT_plc.ReadBytes(DataType.DataBlock, 80, 74, 8);
+                
+                realangle = (float)S7.Net.Types.Double.FromByteArray(bytes1.Skip(0).Take(4).ToArray());
+                fish_full_weight = (float)S7.Net.Types.Double.FromByteArray(bytes1.Skip(4).Take(4).ToArray());
+                sw.Stop();
+                Console.WriteLine("plc数据耗时:" + sw.ElapsedMilliseconds);
+
+                if ((bool)back_high_speed == true)//speed=-80hz
+                {
+                    Console.WriteLine("记录一条数据");
+                    string log = "-45" + "\",\"" + weight.ToString()+ "\",\"" + realangle.ToString() + "\",\""  + fish_full_weight.ToString();
                     LogHelper.WriteLog(log);
                 }
                 else if ((bool)back_low_speed == true)//speed=-40hz
                 {
-                    string log = "-40" + "\",\"" + weight.ToString();
+                    Console.WriteLine("记录一条数据");
+                    string log = "-30" + "\",\"" + weight.ToString() + "\",\"" + realangle.ToString() + "\",\"" + fish_full_weight.ToString();
                     LogHelper.WriteLog(log);
                 }
                 else if ((bool)fall_high_speed == true)////speed=80hz
                 {
-                    string log = "80" + "\",\"" + weight.ToString();
+                    Console.WriteLine("记录一条数据");
+                    string log = "45" + "\",\"" + weight.ToString() + "\",\"" + realangle.ToString() + "\",\"" + fish_full_weight.ToString();
                     LogHelper.WriteLog(log);
                 }
                 else if ((bool)fall_low_speed == true)////speed=40hz
                 {
-                    string log = "40" + "\",\"" + weight.ToString();
+                    Console.WriteLine("记录一条数据");
+                    string log = "30" + "\",\"" + weight.ToString() + "\",\"" + realangle.ToString() + "\",\""  + fish_full_weight.ToString();
                     LogHelper.WriteLog(log);
                 }
-                else if(TB_onpos==true && weight > 70 && lastweight-weight>0)
+                else if(TB_onpos==false && weight > 70 && weight-lastweight>0.1&& realangle>0)
                 {
-                    string log = "0" + "\",\"" + weight.ToString();
+                    Console.WriteLine("记录一条数据");
+                    string log = "0" + "\",\"" + weight.ToString() + "\",\"" + realangle.ToString() + "\",\""  + fish_full_weight.ToString();
                     LogHelper.WriteLog(log);
                 }
                 else
@@ -81,3 +111,6 @@ namespace record_data
 
     }
 }
+
+//var bytes = ZT_plc.ReadBytes(DataType.DataBlock, 5, 4, 4);
+//Double realVariable = S7.Net.Types.Double.FromByteArray(bytes);
